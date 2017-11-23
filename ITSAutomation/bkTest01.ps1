80e3e9a1b4d2ed8c495f9e18115ef89c227b3d3d
bk147 <bkirk@live.dk>
2017-11-23 12:49:07 +0100

    Updated bkTest01.ps1 in Azure Automation.

--- ITSAutomation/bkTest01.ps1
+++ ITSAutomation/bkTest01.ps1
@@ -2,4 +2,8 @@ $info = 'Test from Azure runbook v2 '
 
 $str = $info + '[' + [DateTime]::Now + ']'
 $str
+'Running on: ' + (hostname)
+
 $str | Out-File -FilePath $env:system\_install\AzureRunbook.txt -Append
+
+"Finished writing " + '[' + [DateTime]::Now + ']'
