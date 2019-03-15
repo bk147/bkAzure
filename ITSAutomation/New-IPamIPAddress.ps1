32ec38bc86224abce2036bf20a128750caf5b6b4
Brian Kirkegaard <bkirk@live.dk>
2019-03-15 09:18:14 +0100

    Changed IPam code to use new version and setup.

--- ITSAutomation/New-IPamIPAddress.ps1
+++ ITSAutomation/New-IPamIPAddress.ps1
@@ -21,6 +21,10 @@
 #    IPAddress =     172.18.128.108
 #    DNS =           172.18.16.17
 #}
+#
+# Adding to Azure Dev:
+# Import-AzureRmAutomationRunbook -ResourceGroupName 'AutomationGroup' -AutomationAccountName 'ifsAutomationDev' -Path .\ITSAutomation\New-IPamIPAddress.ps1 -Description 'Getting and reserving an IPAddress from IPAM' -Name New-IPamIPAddress -Type PowerShellWorkflow -Published -Force
+#
 workflow New-IPamIPAddress {
     param (
         #Subnet in CIDR format
@@ -35,17 +39,19 @@ workflow New-IPamIPAddress {
         [string] $Description = 'DescriptionPlaceholder'
     )
 
-    #Static variables - should probably be set as a hashtable in an AutomationVariable...
-    $serviceinfo = Get-AutomationVariable -Name 'IPam_ServiceInfo'
+    #Static variable - should probably be set as a hashtable in an AutomationVariable...
+    $serviceinfo = @{
+        AppID = 'winnsx'
+        Url = 'https://ipam.srv.aau.dk'
+    }
     
     #Get Token for further calls to the API
-    #Maybe the user/password should be stored as Base64 in the above Automation Variable instead of doint the convertion here !?
-    $base64Token = [convert]::ToBase64String([char[]]$serviceinfo.user)
-    $res = Invoke-RestMethod -Method Post -Uri "$($serviceinfo.Url)/api/$($serviceinfo.AppID)/user/" -Headers @{ Authorization="Basic $base64Token" }
+    $cred = Get-AutomationPSCredential -Name 'SVC_IPAM_WinSMA'
+    $res = Invoke-RestMethod -Method Post -Uri "$($serviceinfo.Url)/api/$($serviceinfo.AppID)/user/" -Credential $cred
     $token = $res.data.token
 
     #Get subnet info from a CIDR
-    $subnet = (Invoke-RestMethod -Method Get -Uri "$($serviceinfo.Url)/api/$($serviceinfo.AppID)/subnets/cidr/$CIDRSubnet/" -Headers @{token=$token}).data
+    $subnet = (Invoke-RestMethod -Method Get -Uri "$($serviceinfo.Url)/api/$($serviceinfo.AppID)/subnets/cidr/$CIDRSubnet/" -Headers @{token=$token} -Credential $cred).data
 
     #Prepare the body as json - We have to use InlineScript to remove extra attributes after doing a ConvertTo-Json (PSComputerName,PSShowComputerName,PSSourceJobInstanceId)
     $body = InlineScript {
@@ -55,10 +61,10 @@ workflow New-IPamIPAddress {
         description = $Using:Description
         } | ConvertTo-Json
     }
-    $newip = Invoke-RestMethod -Method Post -Uri "$($serviceinfo.Url)/api/$($serviceinfo.AppID)/addresses/first_free/$($subnet.id)/" -Headers @{token=$token ; 'Content-Type'='application/json'} -Body $body
+    $newip = Invoke-RestMethod -Method Post -Uri "$($serviceinfo.Url)/api/$($serviceinfo.AppID)/addresses/first_free/$($subnet.id)/" -Headers @{token=$token ; 'Content-Type'='application/json'} -Body $body -Credential $cred
 
     #Setup basic variables
-    $ip = $newip.ip
+    $ip = $newip.data
     $length = $subnet.mask
 
     #The following should be stored in IPAM pr. subnet - but for now they are hardcoded
--- ITSAutomation/Remove-IPamIPAddress.ps1
+++ ITSAutomation/Remove-IPamIPAddress.ps1
@@ -3,6 +3,9 @@
 #
 # WARNING: NO ERROR HANDLING IMPLEMENTED YET
 #
+# Adding to Azure Dev:
+# Import-AzureRmAutomationRunbook -ResourceGroupName 'AutomationGroup' -AutomationAccountName 'ifsAutomationDev' -Path .\ITSAutomation\Remove-IPamIPAddress.ps1 -Description 'Removing an IP Address from IPAM' -Name Remove-IPamIPAddress -Type PowerShellWorkflow -Published -Force
+#
 workflow Remove-IPamIPAddress {
     param (
         #IPv4 address to delete
@@ -10,21 +13,24 @@ workflow Remove-IPamIPAddress {
     )
 
     #Static variables - should probably be set as a hashtable in an AutomationVariable...
-    $serviceinfo = Get-AutomationVariable -Name 'IPam_ServiceInfo'
+    $serviceinfo = @{
+        AppID = 'winnsx'
+        Url = 'https://ipam.srv.aau.dk'
+    }
 
     #Get Token for further calls to the API
-    #Maybe the user/password should be stored as Base64 in the above Automation Variable instead of doing the convertion here !?
-    $base64Token = [convert]::ToBase64String([char[]]$serviceinfo.user)
-    $headers=@{ Authorization="Basic $base64Token" }
-    $res = Invoke-RestMethod -Method Post -Uri "$($serviceinfo.Url)/api/$($serviceinfo.AppID)/user/" -Headers $headers
+    $cred = Get-AutomationPSCredential -Name 'SVC_IPAM_WinSMA'
+    $res = Invoke-RestMethod -Method Post -Uri "$($serviceinfo.Url)/api/$($serviceinfo.AppID)/user/" -Credential $cred
     $token = $res.data.token
 
-    $address = (Invoke-RestMethod -Method Get -Uri "$($serviceinfo.Url)/api/$($serviceinfo.AppID)/addresses/search/$IPAddress/" -Headers @{ token=$token }).data
+    $address = (Invoke-RestMethod -Method Get -Uri "$($serviceinfo.Url)/api/$($serviceinfo.AppID)/addresses/search/$IPAddress/" -Headers @{ token=$token } -Credential $cred).data
 
-    $res = Invoke-RestMethod -Method Delete -Uri "$($serviceinfo.Url)/api/$($serviceinfo.AppID)/addresses/$($address.id)/" -Headers @{ token=$token }
+    $res = Invoke-RestMethod -Method Delete -Uri "$($serviceinfo.Url)/api/$($serviceinfo.AppID)/addresses/$($address.id)/" -Headers @{ token=$token } -Credential $cred
 
+    if ($res.success -eq 'True') { $Result = 'Success' } else { $Result = 'Error' }
     @{
-        Result = 'Success'
-        ResultInfo = $res.data
+        Result = $Result
+        IP = $IPAddress
+        ResultInfo = $res.message
     }
 }
