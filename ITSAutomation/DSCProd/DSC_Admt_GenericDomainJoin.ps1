276c446fcc1e04b9820cc94657217fada2452182
Brian Kirkegaard <bkirk@live.dk>
2019-05-06 15:13:34 +0200

    Added admt Generic Domain Join DSC

---
+++ ITSAutomation/DSCProd/DSC_Admt_GenericDomainJoin.ps1
@@ -0,0 +1,48 @@
+configuration DSC_admt_GenericDomainJoin
+{
+    Import-DscResource -ModuleName xCertificate, xComputerManagement, xPSDesiredStateConfiguration, PSDesiredStateConfiguration
+
+    $cred = Get-AutomationPSCredential -Name 'DSC_admt_Domain'
+    $installPath = Get-AutomationVariable -Name 'DSC_InstallTopDirectory'
+
+    Node localhost
+    {
+        xComputer DomainJoin
+        {
+            Name = 'localhost'
+            DomainName = 'admt.aau.dk'
+            Credential = $cred
+            JoinOU = 'OU=Servers,DC=admt,DC=aau,DC=dk'
+        }
+
+        xGroup LocalAdministrators {
+            Ensure = 'Present'
+            GroupName = 'Administrators'
+            MembersToInclude = 'IT CoreMgmt Admins@srv.aau.dk'
+            Credential = $cred
+            DependsOn = '[xComputer]AdmtJoin'
+        }
+
+        xWindowsFeature RSATADTools {
+            Ensure = 'Present'
+            Name = 'RSAT-AD-PowerShell'
+        }
+
+        xCertificateImport vROCertificate {
+            Ensure = 'Present'
+            Thumbprint = '61f224a03e50cf92ad57d594e34be16c1e2886c5'
+            Path = $installPath + '\Certificates\esx-vro01.srv.aau.dk.cer'
+            Location = 'LocalMachine'
+            Store = 'Root'
+            PsDscRunAsCredential = $cred
+        }
+
+#        xCertReq HostCertificate {
+#            Subject = 'test.admt.aau.dk'
+#            CertificateTemplate = 'Machine'
+#            DependsOn = '[xComputer]DomainJoin'
+#        }
+    }
+}
+
+#DSC_Admt_DomainJoin -ConfigurationData .\Config_admt.psd1
